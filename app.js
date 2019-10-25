92ba812ba1a71c037cb74dc4478fd217d012efee
tomson95 <tomaszgr95@interia.pl>
2019-10-25 15:34:23 +0200

    dodanie pliku js

---
+++ app.js
@@ -0,0 +1,22 @@
+
+$(function(){
+
+	$(".central_content")
+    let $des = $(".central_content")
+    
+    $des.find('dd').hide()
+
+    $des.find('dt').on('click', function(){
+		
+        $(this).next().slideToggle()
+    })
+    const btn = document.querySelector(".nav-toggle");
+    const menu = document.querySelector(".page-nav-list");
+
+    btn.addEventListener('click', function() {
+        menu.classList.toggle('show');
+    });
+
+
+    
+})
